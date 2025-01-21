7d0fcd5326b42c7a1ce0a69a087dbedca455cec1
KHADIJA-09 <132650876+KHADIJA-09@users.noreply.github.com>
2025-01-21 12:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function toCase(text) {
   // write your code here
+	let s = text.toLowerCase();
+	let t = text.toUpperCase();
+	let ans = s+"-"+t;
+	return ans
 }
 
 // DO not change the code below
